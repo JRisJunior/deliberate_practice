14f3507993d1198268dad55b4cb7dda43d0faa7a
JRisJunior <jeffmrubin@gmail.com>
2023-02-22 12:16:53 -0600

    hash problem 10 solved

---
+++ hash_practice_problems/problem10.rb
@@ -0,0 +1,11 @@
+# 10. Make a hash to store a shirt's brand, color, and size. Then print each attribute on separate lines.
+
+hash = {
+  brand: "Express",
+  color: "white",
+  size: "medium"
+}
+
+p hash[:brand]
+p hash[:color]
+p hash[:size]
