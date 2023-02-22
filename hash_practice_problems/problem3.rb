55b3ae17d9183ed3a79ee723a58b88da452bcd83
JRisJunior <jeffmrubin@gmail.com>
2023-02-22 11:50:48 -0600

    hash problem 3 solved

---
+++ hash_practice_problems/problem3.rb
@@ -0,0 +1,11 @@
+# 3. Make a hash to store prices for 3 different menu items. Then add a new menu item and price and print the hash to see the result.
+
+hash = {
+  fries: 3,
+  burger: 10,
+  coke: 2.5
+}
+
+hash[:sloppy_joe] = 9
+
+p hash
