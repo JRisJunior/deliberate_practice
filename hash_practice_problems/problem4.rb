206b2fbec5f780413503fb3bf05bdd44853646cc
JRisJunior <jeffmrubin@gmail.com>
2023-02-22 12:01:18 -0600

    hash problem 4 solved

---
+++ hash_practice_problems/problem4.rb
@@ -0,0 +1,14 @@
+# 4. Make a hash to store a book's title, author, number of pages, and language. Then print each attribute on separate lines.
+
+hash = {
+  title: "Mistborn",
+  author: "Brandon Sanderson",
+  page_count: 900,
+  language: "English"
+}
+
+p hash[:title]
+p hash[:author]
+p hash[:page_count]
+p hash[:language]
+
