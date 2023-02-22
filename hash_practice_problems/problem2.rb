b2d26da887ee09fe257b08be7719aa61b400b584
JRisJunior <jeffmrubin@gmail.com>
2023-02-22 11:45:53 -0600

    problem 2 solved

---
+++ hash_practice_problems/problem2.rb
@@ -0,0 +1,18 @@
+# 2. Make an array of hashes to store the first name and last name for 3 different people. Then print out the first person's info.
+
+hash = [
+  {
+    fname: "Moe",
+    lname: "Stooge"
+  },
+  {
+    fname: "Larry",
+    lname: "Stooge"
+  },
+  {
+    fname: "Curly",
+    lname: "Stooge" 
+  }
+]
+
+p hash[0]
