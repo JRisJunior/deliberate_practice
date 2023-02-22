a6696aa3095cea3d4f98c6d3e5276cb2b3b4ef59
JRisJunior <jeffmrubin@gmail.com>
2023-02-22 12:10:20 -0600

    hash problem 7 solved

---
+++ hash_practice_problems/problem7.rb
@@ -0,0 +1,11 @@
+# 7. Make a hash to store a laptop's brand, model, and year. Then print each attribute on separate lines.
+
+hash = {
+  brand: "Apple",
+  model: "Macbook Air",
+  year: 2020
+}
+
+p hash[:brand]
+p hash[:model]
+p hash[:year]
