1556ba7bc3d9d9d879ebfe8d70161eae4709c05d
JRisJunior <jeffmrubin@gmail.com>
2023-02-22 12:15:26 -0600

    hash problem 9 solved

---
+++ hash_practice_problems/problem9.rb
@@ -0,0 +1,11 @@
+# 9. Make a hash to store definitions for 2 different words. Then add a new word and definition and print the hash to see the result.
+
+hash = {
+  because: "for the reason that; since",
+  whisper: "speak very softly",
+  duck: "a waterbird with webbed feet"
+}
+
+hash[:think] = "have a particular opinion"
+
+p hash
