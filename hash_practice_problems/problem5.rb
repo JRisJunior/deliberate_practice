ff14d9b80f0830d088e1481c4fa483829b74a254
JRisJunior <jeffmrubin@gmail.com>
2023-02-22 12:03:42 -0600

    hash problem 5 solved

---
+++ hash_practice_problems/problem5.rb
@@ -0,0 +1,18 @@
+# 5. Make an array of hashes to store the title and author for 3 different books. Then print out the third book's author.
+
+hash = [
+  {
+    title: "Maus",
+    author: "Mouse"
+  },
+  {
+    title: "Mistborn",
+    author: "Sanderson"
+  },
+  {
+    title: "Arthur",
+    author: "Turtle"
+  }
+]
+
+p hash[2][:author]
