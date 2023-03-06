36d7835361d4a02863f3ed5de4216df4266720a2
JRisJunior <jeffmrubin@gmail.com>
2023-03-06 15:51:03 -0600

    function problem 9 solved

---
+++ functions_practice_problems/problem9.rb
@@ -0,0 +1,8 @@
+# 9. Write a function that takes in 3 numbers and returns the average (the sum divided by 3.0). Then run the function and print the result.
+
+def average(x, y, z)
+  answer = (x + y + z) / 3
+  answer
+end
+
+p average(10, 20, 65)
