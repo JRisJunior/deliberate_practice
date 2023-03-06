bca898ce7246dc0e9c1ceafc97aad2453a3eda32
JRisJunior <jeffmrubin@gmail.com>
2023-03-06 15:52:42 -0600

    function problem 10 solved

---
+++ functions_practice_problems/problem10.rb
@@ -0,0 +1,9 @@
+# 10. Write a function that takes in a number and returns the number times 10 plus 30. Then run the function and print the result.
+
+def math(number)
+  product = number * 10
+  sum = product + 30
+  sum
+end
+
+p math(35)
