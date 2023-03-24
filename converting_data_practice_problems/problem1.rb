b95d989feb4fffb37ea72b203db527fc62447756
JRisJunior <jeffmrubin@gmail.com>
2023-03-24 11:56:30 -0500

    start convert data problems

---
+++ converting_data_practice_problems/problem1.rb
@@ -0,0 +1,13 @@
+#  1. Convert an array of arrays into a hash.
+#     For example, [[1, 3], [8, 9], [2, 16]] becomes {1 => 3, 8 => 9, 2 => 16}.
+
+
+
+def arr_to_hash(arr)
+  
+end
+
+
+
+
+array = [[1, 3], [8, 9], [2, 16]]
