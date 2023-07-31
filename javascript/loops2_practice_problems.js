6de6674dd8cdc639588c34918cc0ab26d509f612
JRisJunior <jeffmrubin@gmail.com>
2023-07-31 11:50:50 -0500

    js loop prob 7 solve

--- javascript/loops2_practice_problems.js
+++ javascript/loops2_practice_problems.js
@@ -34,18 +34,30 @@
 
 // # 5. Write a while loop that prints the number 9000 ten times.
 
-let i = 0;
-var number = 9000;
-while (i < 10) {
-  console.log(number);
-  i++;
-}
+// let i = 0;
+// var number = 9000;
+// while (i < 10) {
+//   console.log(number);
+//   i++;
+// }
 
 // # 6. Write a while loop that asks the user to enter a number and will run forever until the user enters a number greater than 10.
 
 // # 7. Write a while loop that prints the numbers 50 to 70.
 
+// let number = 50;
+// while (number <= 70) {
+//   console.log(number);
+//   number++;
+// }
+
 // # 8. Write a while loop that prints the phrase "Around the world" 144 times.
+
+let i = 0;
+while (i < 144) {
+  console.log("Around the world");
+  i++;
+}
 
 // # 9. Write a while loop that asks the user to enter a word and will run forever until the user enters a word with more than 5 letters.
 
