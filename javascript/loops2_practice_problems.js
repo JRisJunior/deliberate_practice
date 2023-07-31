e71c8c4b0dc57666716ed4f054f6aa5848bacdc4
JRisJunior <jeffmrubin@gmail.com>
2023-07-31 10:57:43 -0500

    js loop prob 2 solve

--- javascript/loops2_practice_problems.js
+++ javascript/loops2_practice_problems.js
@@ -1,11 +1,16 @@
 // # 1. Write a while loop to print the numbers 1 through 10.
 
-let i = 0;
-while (i < 10) {
-  const number = 1;
-  console.log(number + i);
-  i++;
-}
+// let i = 0;
+// while (i < 10) {
+//   const number = 1;
+//   console.log(number + i);
+//   i++;
+// }
 
 // # 2. Write a while loop that prints the word "hello" 5 times.
 
+let i = 0;
+while (i < 5) {
+  console.log("hello");
+  i++;
+}
